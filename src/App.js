35604381d340ac0a74a71dc34f7cc489e2c2f517
Wiand1515 <marofual@gmail.com>
2021-06-30 23:30:59 -0400

    Comit estetico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import FetchRM from './components/FetchRM';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-
+    <div className="background">
+      <Navbar/>
+      
+      <div className="container">
+      
       <h1>Hola mundo</h1>
       <FetchRM/>
+
+      </div>
       
     </div>
   )
